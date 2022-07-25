0f97bf1836e1d44a24c804cc9a5da4b54ac66d40
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-07-25 20:59:09 -0300

    subindo projeto completo

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Comecei na Indústria onde fiquei por nove anos. Entrei na escrituração fiscal e posteriormente fui designado a ser o líder da gestão e controle de estoque e produção. Coordenei equipes em quase todo processo da Indústria.
+      </p>
+      <p>
+        Atualmente sou CEO das minhas empresas em transição de carreira. Pretendo trabalhar somente como desenvolvedor. É esse meu sonho e o que tanto almejo para meu futuro como profissional.
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,15 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Já fiz vários projetos profissionais. Todos estudando, fiz um projeto Integrador na Digital House durante o período de estudo. Em 10 meses de muito estudo fiz 05 projetos completos, com Front e Back-end. E cerca de 10 projetos front-end.
+      </p>
+      <a href="https://github.com/MarcusViniciusAbdalla" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
